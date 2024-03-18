ec62e0124fc769f5da7efa2f4d2dd9e54722fc1c
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-03-18 20:11:28 +0000

    feat: Add id's from other domains

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,12 @@
-import { randomUUID } from "crypto"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface AnswerProps {
+    authorId: UniqueEntityID
+    questionId: UniqueEntityID
     content:string
-    authorId: string
-    questionId:string
+    createdAt: Date
+    updatedAt?: Date
 }
 
 
