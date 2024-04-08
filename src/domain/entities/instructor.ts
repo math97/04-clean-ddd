4fef2529c0e63a993ef7d3486d075c207fb11812
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-04-08 22:11:22 +0100

    chore: Refactor using optioanl options to create entities

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,10 +1,17 @@
-import { randomUUID } from "node:crypto"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface InstructorProps {
     name: string
   }
 
 export class Instrucutor extends Entity<InstructorProps> {
+  static create(
+    props: InstructorProps,
+    id?: UniqueEntityID,
+  ) {
+    const instructor = new Instrucutor(props, id)
 
+    return instructor
+  }
 }
