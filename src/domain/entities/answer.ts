485a5903268eed2800f0dc40d88f5d08290ed0a8
Matheus DEV <matheusdev@L-LLQPKP2GFG.lan>
2024-04-08 23:59:49 +0100

    chore: Eslint config for the entire project

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,15 +1,14 @@
-import { Entity } from "@/core/entities/entity"
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
-import { Optional } from "@/core/types/optional"
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
 
 interface AnswerProps {
-    authorId: UniqueEntityID
-    questionId: UniqueEntityID
-    content:string
-    createdAt: Date
-    updatedAt?: Date
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
 }
-
 
 export class Answer extends Entity<AnswerProps> {
   get authorId() {
@@ -21,8 +20,9 @@ export class Answer extends Entity<AnswerProps> {
   }
 
   get content() {
-      return this.props.content
+    return this.props.content
   }
+
   get createdAt() {
     return this.props.createdAt
   }
@@ -32,12 +32,9 @@ export class Answer extends Entity<AnswerProps> {
   }
 
   get excerpt() {
-    return this.content
-      .substring(0, 120)
-      .trimEnd()
-      .concat('...')
+    return this.content.substring(0, 120).trimEnd().concat('...')
   }
-  
+
   private touch() {
     this.props.updatedAt = new Date()
   }
@@ -45,17 +42,20 @@ export class Answer extends Entity<AnswerProps> {
   set content(content: string) {
     this.props.content = content
     this.touch()
-  }    
-  
+  }
+
   static create(
-      props: Optional<AnswerProps, 'createdAt'>,
-      id?: UniqueEntityID
-    ) {
-      const answer = new Answer({
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const answer = new Answer(
+      {
         ...props,
         createdAt: new Date(),
-      }, id)
-  
-      return answer
-    }
+      },
+      id,
+    )
+
+    return answer
+  }
 }
--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,15 +1,12 @@
-import { Entity } from "@/core/entities/entity"
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface InstructorProps {
-    name: string
-  }
+  name: string
+}
 
 export class Instrucutor extends Entity<InstructorProps> {
-  static create(
-    props: InstructorProps,
-    id?: UniqueEntityID,
-  ) {
+  static create(props: InstructorProps, id?: UniqueEntityID) {
     const instructor = new Instrucutor(props, id)
 
     return instructor
